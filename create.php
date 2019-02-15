cd33a188eba8d067fcecc4ab74341cd8421289a0
Steven Lei <stevenplus@gmail.com>
2019-02-15 20:44:15 +0800

    refactor a little bit to saveNote()

--- create.php
+++ create.php
@@ -1,16 +1,15 @@
 <?php
 
+require './functions.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	$title = $_POST['title'];
 	$content = $_POST['content'];
 
 	$filename = time() . '.txt';
-	$filepath = './notes/' . $filename;
-
-	$content = $title . "\n" . $content;
-
-	file_put_contents($filepath, $content, LOCK_EX);
+	
+	saveNote($filename, $title, $content);
 
 	header('Location: /index.php');
 
--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function getFiles($path)
 	return array_diff(scandir($path), array('.', '..'));
 }
 
+function saveNote($filename, $title, $content)
+{
+	$filepath = __DIR__ . '/notes/' . $filename;
+	$data = $title . "\n" . $content;
+
+	file_put_contents($filepath, $data, LOCK_EX);
+}
