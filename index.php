7b862ab54b1bca15539d513fabed1b2211aaf356
Steven Lei <stevenplus@gmail.com>
2019-02-16 00:23:54 +0800

    try out if the Connection and QueryBuilder works

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 require 'functions.php';
 
+require 'database/Connection.php';
+require 'database/QueryBuilder.php';
+
+$pdo = Connection::make();
+$query = new QueryBuilder($pdo);
+$notes = $query->fetchAll('notes');
+
+print_r($notes);
+exit;
+
 $notes = getNotes();
 
 if (isset($_GET['id']))
