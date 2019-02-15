aadf464223f1e622da466931d92c263e684a7b79
Steven Lei <stevenplus@gmail.com>
2019-02-16 01:33:23 +0800

    add fetchOne() to QueryBuilder

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -16,4 +16,22 @@ class QueryBuilder
 
 		return $stmt->fetchAll(PDO::FETCH_OBJ);
 	}
+
+	public function fetchOne($table, $criteria = [])
+	{
+		$keys = array_keys($criteria);
+		$criteria_sql = [];
+
+		foreach ($keys as $key)
+		{
+			array_push($criteria_sql, "`{$key}` = :{$key}");
+		}
+
+		$criteria_sql = join(' AND ', $criteria_sql);
+
+		$stmt = $this->pdo->prepare("SELECT * FROM {$table} WHERE {$criteria_sql}");
+		$stmt->execute($criteria);
+
+		return $stmt->fetch(PDO::FETCH_OBJ);
+	}
 }
