61a3d1ada6e9cd929419d40043a386eb8b4dcfb4
Steven Lei <stevenplus@gmail.com>
2019-02-16 00:59:45 +0800

    moving files as controllers

---
+++ controllers/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'functions.php';
+
+// $notes = $database->fetchAll('notes');
+
+$notes = getNotes();
+
+if (isset($_GET['id']))
+{
+	$the_note = getNoteById($_GET['id']);
+}
+
+require 'views/index.view.php';
