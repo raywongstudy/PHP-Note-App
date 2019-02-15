4e50765329d704583590ee8c9b8d01dc92888039
Steven Lei <stevenplus@gmail.com>
2019-02-15 20:41:21 +0800

    save the note contents via create.php

--- create.php
+++ create.php
@@ -5,7 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	$title = $_POST['title'];
 	$content = $_POST['content'];
 
-	var_dump($title, $content);
+	$filename = time() . '.txt';
+	$filepath = './notes/' . $filename;
+
+	$content = $title . "\n" . $content;
+
+	file_put_contents($filepath, $content, LOCK_EX);
+
+	header('Location: /index.php');
 
 	exit;
 }
