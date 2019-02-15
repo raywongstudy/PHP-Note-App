b57ae6805e488ba5fdf9f57786afb802697739b4
Steven Lei <stevenplus@gmail.com>
2019-02-16 01:02:13 +0800

    move REQUEST_URI into Router resolve()

--- Router.php
+++ Router.php
@@ -9,8 +9,14 @@ class Router
 		self::$routes[$uri] = $file;
 	}
 
-	public static function resolve($uri)
+	public static function resolve()
 	{
+		$uri = $_SERVER['REQUEST_URI'];
+
+		if (strlen($uri) > 1) {
+			$uri = rtrim($uri, '/');
+		}
+
 		if (isset(self::$routes[$uri]))
 		{
 			return self::$routes[$uri];
--- index.php
+++ index.php
@@ -8,6 +8,4 @@ Router::bind('/', 'controllers/index.php');
 Router::bind('/create', 'controllers/create.php');
 Router::bind('/edit', 'controllers/edit.php');
 
-$uri = $_SERVER['REQUEST_URI'];
-
 require Router::resolve($uri);
